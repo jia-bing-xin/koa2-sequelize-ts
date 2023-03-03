3781619e39d20d06d83704028487a92b085efd74
jia-bing-xin <441026068@qq.com>
2023-03-03 08:50:36 +0800

    分离数据库

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,13 @@
+const { createUser } = require('@/service/user.service')
 class UserController {
     async register(ctx: any, next: any) {
-        console.log('用户注册成功', ctx.request.body)
-        ctx.body = '用户注册成功'
+        // 1.获取数据
+        // const { user_name, password } = ctx.request.body
+        // 2.操作数据库
+        const res = await createUser(ctx.request.body)
+        console.log(res)
+        // 3.返回结果
+        ctx.body = ctx.request.body
     }
     async login(ctx: { body: string }, next: any) {
         ctx.body = '用户登录成功'
---
+++ src/service/user.service.ts
@@ -0,0 +1,8 @@
+class UserService {
+    async createUser(data: any) {
+        return data
+    }
+}
+const userService = new UserService()
+module.exports = userService
+export { }
